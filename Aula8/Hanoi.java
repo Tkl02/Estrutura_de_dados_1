60c2386c93465fa4ee17ae56f46045ea560e71f0
Leonardo Faustino <leonardofaustino0@gmail.com>
2022-05-21 12:39:23 -0300

    .

--- Aula8/Hanoi.java
+++ Aula8/Hanoi.java
@@ -1,23 +1,30 @@
 package Aula8;
+import java.lang.Math;
 
 public class Hanoi{
     public static void main(String[] args) {
       long tempoInicial = System.currentTimeMillis();
-      int discos = 40; //1,10,20,30,40,41
+      int discos = 1; //1,10,20,30,40,41
+      int p;
       hanoi(discos,'A', 'B', 'C');
       long tempoFinal = System.currentTimeMillis();
       long tempoTotal = tempoFinal - tempoInicial;
-      int miliSegundos = (int) tempoTotal%1000;
-      int segundos = (int)( tempoTotal / 1000 ) % 60;     
-      int minutos  = (int)( tempoTotal / 60000 ) % 60;    
-      int horas    = (int) tempoTotal / 3600000;          
-      System.out.println( String.format( "%02d:%02d:%02d:%03d", horas, minutos, segundos, miliSegundos) );
-      
+      int miliSeg     = (int) tempoTotal%1000;
+      int seg         = (int)( tempoTotal / 1000 ) % 60;     
+      int min         = (int)( tempoTotal / 60000 ) % 60;    
+      int hr          = (int) tempoTotal / 3600000;         
+      System.out.println( String.format( "%02d:%02d:%02d:%03d", hr, min, seg, miliSeg));
+
+      p = (int) (Math.pow(2, discos) - 1);
+
+      System.out.println("movimentos: " + p);
     }
   
     private static void hanoi(int discos, char origem, char aux, char destino) {
       if(discos > 0){
+
         hanoi(discos - 1, origem, destino, aux);
+        System.out.printf("%s -> %s\n", origem, destino);
         hanoi(discos-1, aux, origem, destino);
       }
     }
