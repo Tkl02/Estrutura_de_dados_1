03d194f183ec303184a6340db11bb46b4d1fd1c3
Leonardo Faustino <leonardofaustino0@gmail.com>
2022-06-06 08:03:42 -0300

    .

--- Aula9/main.java
+++ Aula9/main.java
@@ -2,7 +2,7 @@ package Aula9;
 
 import java.util.Scanner;
 
-public class atv1 {
+public class main {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
