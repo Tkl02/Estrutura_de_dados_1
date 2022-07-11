88a94fdbe23d7aa69a11936e4c7a25b869995804
Tkl02 <leonardofaustino0@gmail.com>
2022-07-11 09:47:35 -0300

    .

--- aula11/Fila.java
+++ aula11/Fila.java
@@ -4,6 +4,66 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Fila {
+
+    public static void main(String [] args){
+        Fila naoAtendidas = new Fila();
+        Fila atendidas = new Fila();
+        Random gerador = new Random();
+        ArrayList<String> sexos = new ArrayList<String>();
+        sexos.add("Masculino");
+        sexos.add("Feminino");
+        int id = 1;
+        for (int i = 0; i < 10; i++) {
+                for(int h = 0; h < 5; h++){
+                    String sexo = sexos.get(gerador.nextInt(2));
+                    int idade = gerador.nextInt(59)+1;
+                    naoAtendidas.inserirInicio(id, sexo, idade, false, false, false, 0);
+                    id++;
+                }
+                for(int h = 0; h < 3; h++){
+                    String sexo = sexos.get(gerador.nextInt(2));
+                    int idade = gerador.nextInt(60)+60;
+                    naoAtendidas.inserirInicio(id, sexo, idade, false, false, false, 1);
+                    id++;
+                }
+                String sexo = sexos.get(gerador.nextInt(2));
+                int idade = gerador.nextInt(59)+1;
+                naoAtendidas.inserirInicio(id, sexo, idade, false, false, true, 2);
+                id++;
+                sexo = sexos.get(1);
+                idade = gerador.nextInt(41)+8;
+                naoAtendidas.inserirInicio(id, sexo, idade, true, true, false, 3);
+                id++;
+                naoAtendidas = ordenar(naoAtendidas);
+                for (int f = 0; f <= i ; f++) {
+                    Celula atendido = naoAtendidas.retirarInicio();
+                    atendidas.inserirFim(atendido.id, atendido.sexo, atendido.idade, atendido.gestante, atendido.lactante, atendido.necessidadeEspecial, atendido.prioridade);
+                }
+            } 
+            
+        System.out.println("Pessoas atendidas========"+ atendidas); 
+        System.out.println("Pessoas não atendida====="+ naoAtendidas);     
+    }
+    public static Fila ordenar(Fila naoAtendidas){
+        if (naoAtendidas.tamanho > 1) {
+            for (int k = 1; k < naoAtendidas.tamanho; k++) {
+                if (naoAtendidas.selecionarIndice(k).prioridade >
+                    naoAtendidas.selecionarIndice(k - 1).prioridade ) {
+                    Celula aux = new Celula();
+                    Celula aux2 = new Celula();
+                    aux = naoAtendidas.selecionarIndice(k-1);
+                    aux2 = naoAtendidas.selecionarIndice(k);
+                    naoAtendidas.inserirIndice(k-1, aux2.id, aux2.sexo, aux2.idade, aux2.gestante, aux2.lactante, aux2.necessidadeEspecial, aux2.prioridade);
+                    naoAtendidas.inserirIndice(k, aux.id, aux.sexo, aux.idade, aux.gestante, aux.lactante, aux.necessidadeEspecial, aux.prioridade);
+                    naoAtendidas.retirarIndice(k-1);
+                    naoAtendidas.retirarIndice(k-1);
+                    return naoAtendidas;
+                }
+            }
+        }
+        return naoAtendidas;
+    }
+
     Celula inicio = null;
     int tamanho = 0;
 
@@ -107,63 +167,5 @@ public class Fila {
             local = local.proximo; 
         }
         return str;
-    }
-    public static void main(String [] args){
-        Fila naoAtendidas = new Fila();
-        Fila atendidas = new Fila();
-        Random gerador = new Random();
-        ArrayList<String> sexos = new ArrayList<String>();
-        sexos.add("Masculino");
-        sexos.add("Feminino");
-        int id = 1;
-        for (int i = 0; i < 10; i++) {
-                for(int h = 0; h < 5; h++){
-                    String sexo = sexos.get(gerador.nextInt(2));
-                    int idade = gerador.nextInt(59)+1;
-                    naoAtendidas.inserirInicio(id, sexo, idade, false, false, false, 0);
-                    id++;
-                }
-                for(int h = 0; h < 3; h++){
-                    String sexo = sexos.get(gerador.nextInt(2));
-                    int idade = gerador.nextInt(60)+60;
-                    naoAtendidas.inserirInicio(id, sexo, idade, false, false, false, 1);
-                    id++;
-                }
-                String sexo = sexos.get(gerador.nextInt(2));
-                int idade = gerador.nextInt(59)+1;
-                naoAtendidas.inserirInicio(id, sexo, idade, false, false, true, 2);
-                id++;
-                sexo = sexos.get(1);
-                idade = gerador.nextInt(41)+8;
-                naoAtendidas.inserirInicio(id, sexo, idade, true, true, false, 3);
-                id++;
-                naoAtendidas = ordenar(naoAtendidas);
-                for (int f = 0; f <= i ; f++) {
-                    Celula atendido = naoAtendidas.retirarInicio();
-                    atendidas.inserirFim(atendido.id, atendido.sexo, atendido.idade, atendido.gestante, atendido.lactante, atendido.necessidadeEspecial, atendido.prioridade);
-                }
-            } 
-            
-        System.out.println("Pessoas atendidas: =========="+ atendidas); 
-        System.out.println("Pessoas não atendidas: =============="+ naoAtendidas);     
-    }
-    public static Fila ordenar(Fila naoAtendidas){
-        if (naoAtendidas.tamanho > 1) {
-            for (int k = 1; k < naoAtendidas.tamanho; k++) {
-                if (naoAtendidas.selecionarIndice(k).prioridade >
-                    naoAtendidas.selecionarIndice(k - 1).prioridade ) {
-                    Celula aux = new Celula();
-                    Celula aux2 = new Celula();
-                    aux = naoAtendidas.selecionarIndice(k-1);
-                    aux2 = naoAtendidas.selecionarIndice(k);
-                    naoAtendidas.inserirIndice(k-1, aux2.id, aux2.sexo, aux2.idade, aux2.gestante, aux2.lactante, aux2.necessidadeEspecial, aux2.prioridade);
-                    naoAtendidas.inserirIndice(k, aux.id, aux.sexo, aux.idade, aux.gestante, aux.lactante, aux.necessidadeEspecial, aux.prioridade);
-                    naoAtendidas.retirarIndice(k-1);
-                    naoAtendidas.retirarIndice(k-1);
-                    return naoAtendidas;
-                }
-            }
-        }
-        return naoAtendidas;
     }
 }
